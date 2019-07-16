c979f21ed1ab74c2c28e1c31cdc84e3d36c7d28f
Sandy <sandy.jernigan@gmail.com>
2019-07-15 21:58:02 -0400

    Setup getAction which will fetch data using axios from the api and return a success or a failed result

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,4 +12,20 @@ export const GET_FAILED = 'GET_FAILED'
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+// remember that now we have control over our thunk-based action creator
+
+export function getAction() {
+	// can return a function because we're using redux-thunk
+	return (dispatch) => { // receives direct access to the dispatcher
+		// enter the "loading" state
+		dispatch({ type: GET_START })
+
+		axios.get('https://swapi.co/api/people/')
+			.then((res) => {
+				dispatch({ type: GET_SUCCESS, payload: res.data })
+			})
+			.catch((err) => {
+				dispatch({ type: GET_FAILED, payload: err.response.data })
+			})
+	}
+}
